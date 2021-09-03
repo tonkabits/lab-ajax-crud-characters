ae9bdfc734bc9733343d1583560a504bf4972985
Tonkabits <tonkabits@gmail.com>
2021-09-02 22:08:28 -0400

    WIP

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,56 @@
+// const axios = require('axios')
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    const url = `http://localhost:8000/characters`
+    axios
+      .get(url)
+      .then(response => {
+          console.log(response)
+      })
+      .catch()
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    const url = `http://localhost:8000/characters/${id}`
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch()
   }
 
   createOneRegister () {
-
+    const url = `http://localhost:8000/characters`
+    axios
+    .post(url)
+      .then(response => {
+        console.log(response)
+      })
+    .catch()
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id) {
+    const url = `http://localhost:8000/characters/${id}`
+    axios
+      .patch(url)
+      .then(response => {
+        console.log(response)
+      })
+      .catch()
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    const url = `http://localhost:8000/characters/${id}`
+    axios
+      .delete(url)
+      .then(response => {
+        console.log(response)
+      })
+      .catch()
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,15 +2,17 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+   const id = document.getElementById('character-id').value
+    charactersAPI.getOneRegister(id)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const id = document.getElementById('character-id').value
+    charactersAPI.deleteOneRegister(id)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
