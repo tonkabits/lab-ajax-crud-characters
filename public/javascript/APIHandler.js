865dde7d91d51e597922733f75866ba1c774381e
Tonkabits <tonkabits@gmail.com>
2021-09-03 16:25:53 -0400

    doneish

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -10,6 +10,25 @@ class APIHandler {
       .get(url)
       .then(response => {
           console.log(response)
+      
+        document.getElementById('characters-container').innerHTML = ''
+        for (let character of response.data) {
+          
+          let card = document.createElement("div")
+          card.classList.add('character-info')
+          let char = `
+          
+            <div class="name">id: <span style="color: #d8b362;">${character.id}</span></div>
+            <div class="name">name: <span style="color: #d8b362;">${character.name}</span></div>
+            <div class="occupation">occupation: <span style="color: #d8b362;">${character.occupation}</span></div>
+            <div class="cartoon">is a Cartoon?: <span style="color: #d8b362;">${character.cartoon}</span></div>
+            <div class="weapon">weapon: <span style="color: #d8b362;">${character.weapon}</span></div>
+          
+          `
+          console.log(card)
+          card.innerHTML = char
+          document.getElementById('characters-container').appendChild(card)
+        }
       })
       .catch()
   }
@@ -20,24 +39,40 @@ class APIHandler {
       .get(url)
       .then(response => {
         console.log(response.data)
+        document.getElementById('characters-container').innerHTML = ''
+        let card = document.createElement("div")
+        card.classList.add('character-info')
+        let char = `
+          
+            <div class="name">id: <span style="color: #d8b362;">${response.data.id}</span></div>
+            <div class="name">name: <span style="color: #d8b362;">${response.data.name}</span></div>
+            <div class="occupation">occupation: <span style="color: #d8b362;">${response.data.occupation}</span></div>
+            <div class="cartoon">is a Cartoon?: <span style="color: #d8b362;">${response.data.cartoon}</span></div>
+            <div class="weapon">weapon: <span style="color: #d8b362;">${response.data.weapon}</span></div>
+          
+          `
+        console.log(card)
+        card.innerHTML = char
+        document.getElementById('characters-container').appendChild(card)
       })
       .catch()
   }
 
-  createOneRegister () {
+  createOneRegister (name, occupation, weapon, cartoon) {
     const url = `http://localhost:8000/characters`
+    // console.log(name, occupation, weapon, cartoon)
     axios
-    .post(url)
+    .post(url, {name, occupation, weapon, cartoon})
       .then(response => {
         console.log(response)
       })
     .catch()
   }
 
-  updateOneRegister (id) {
+  updateOneRegister(id, name, occupation, weapon, cartoon ) {
     const url = `http://localhost:8000/characters/${id}`
     axios
-      .patch(url)
+      .patch(url, { name, occupation, weapon, cartoon })
       .then(response => {
         console.log(response)
       })
@@ -50,6 +85,14 @@ class APIHandler {
       .delete(url)
       .then(response => {
         console.log(response)
+        if(response.status === 200){
+          console.log('deleted correctly')
+          document.getElementById('delete-one').style.backgroundColor = "#4ED993"
+      
+        } else if (!response.status === 404){
+          console.log('error while deleting')
+          document.getElementById('delete-one').style.backgroundColor = "#F03E3D"
+        }
       })
       .catch()
   }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -11,15 +11,25 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-    const id = document.getElementById('character-id').value
+    const id = document.getElementById('character-id-delete').value
+    console.log(id)
     charactersAPI.deleteOneRegister(id)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    const id = document.getElementById('id-edit').value
+    const name = document.getElementById('name-edit').value
+    const occupation = document.getElementById('occupation-edit').value
+    const weapon = document.getElementById('weapon-edit').value
+    const cartoon = document.getElementById('cartoon-edit').value
+    charactersAPI.updateOneRegister(id, name, occupation, weapon, cartoon)
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('new-character-form').addEventListener('submit', function (event) {  
+    const name = document.getElementById('name-create').value
+    const occupation = document.getElementById('occupation-create').value
+    const weapon = document.getElementById('weapon-create').value
+    const cartoon = document.getElementById('cartoon-create').value
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon)
   });
 });
